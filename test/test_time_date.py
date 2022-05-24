79ff5cd2dd20a1c12485231615a3223f27769e58
ChenxiQ <cq53@cornell.edu>
2022-05-23 23:32:22 -0400

    Add test for python time formatting

---
+++ test/test_time_date.py
@@ -0,0 +1,11 @@
+import time
+
+loggerName = "Chenxi"
+fieldNumber = "5"
+rowNumber = "2"
+
+captureDate = str(time.strftime("%Y-%m-%d", time.localtime(time.time())))
+csvFilePrefix = "/home/pi/MultisensorCropMonitoringPlatform/data/lidar"
+csvFilePath = "{}/Field{}_{}_Row{}.csv".format(csvFilePrefix, fieldNumber, captureDate, rowNumber)
+
+print(csvFilePath)
