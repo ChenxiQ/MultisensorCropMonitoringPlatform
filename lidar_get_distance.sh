f2f3474201f0731b76947142d4033299dcc44f1f
ChenxiQ <cq53@cornell.edu>
2022-05-26 16:17:11 -0400

    Add script for upload Arduino code for lidar

---
+++ lidar_get_distance.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+arduino-cli compile --fqbn arduino:avr:mega /home/pi/MultisensorCropMonitoringPlatform/lidar/lidar_get_distance/lidar_get_distance.ino
+
+arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:mega /home/pi/MultisensorCropMonitoringPlatform/lidar/lidar_get_distance/lidar_get_distance.ino
+echo "Finish uploading sketch lidar_get_distance"
