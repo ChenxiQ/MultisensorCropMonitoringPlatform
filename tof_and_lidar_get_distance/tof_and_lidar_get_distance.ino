84dc46c2ee445096863fe03d24b3143632fa5f43
ChenxiQ <cq53@cornell.edu>
2022-06-14 10:40:39 -0400

    Add comments

--- tof_and_lidar_get_distance/tof_and_lidar_get_distance.ino
+++ tof_and_lidar_get_distance/tof_and_lidar_get_distance.ino
@@ -17,9 +17,11 @@ void setup() {
     digitalWrite(SCL, LOW);
     digitalWrite(SDA, LOW);
 
+    // Initialize Garmin Lidar
     myLidarLite.configure(0);
-    distanceSensor.setDistanceModeShort();
 
+    // Initialize ToF distance sensor
+    distanceSensor.setDistanceModeShort();
     // Begin returns 0 on a good init
     if (distanceSensor.begin() != 0) {
         Serial.println("Sensor failed to begin. Please check wiring. Freezing...");
@@ -30,6 +32,7 @@ void setup() {
 }
 
 void loop() {
+    // Garmin Lidar get distance
     if (myLidarLite.getBusyFlag() == 0) {
         myLidarLite.takeRange();
         distance = myLidarLite.readDistance();
@@ -40,17 +43,16 @@ void loop() {
 
     Serial.print(" ");
 
+    // ToF distance sensor get distance
     // Write configuration bytes to initiate measurement
     distanceSensor.startRanging();
     while (!distanceSensor.checkForDataReady()) {
         delay(1);
     }
-    
     // Get the result of the measurement from the sensor
     int distance = distanceSensor.getDistance();
     distanceSensor.clearInterrupt();
     distanceSensor.stopRanging();
-
     // Serial.print("ToF distance: ");
     Serial.println(distance/10);
 
