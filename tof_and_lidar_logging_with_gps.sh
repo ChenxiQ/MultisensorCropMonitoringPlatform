b34f4b7d549a50cad33d438e5b16d8d137cf67f1
ChenxiQ <cq53@cornell.edu>
2022-06-14 16:19:31 -0400

    Add code for logging two sensors data

---
+++ tof_and_lidar_logging_with_gps.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+arduino-cli compile --fqbn arduino:avr:mega /home/pi/MultisensorCropMonitoringPlatform/tof_and_lidar/tof_and_lidar_get_distance/tof_and_lidar_get_distance.ino
+
+arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:mega /home/pi/MultisensorCropMonitoringPlatform/tof_and_lidar/tof_and_lidar_get_distance/tof_and_lidar_get_distance.ino
+echo "Finish uploading sketch lidar_get_distance"
+
+python3 /home/pi/MultisensorCropMonitoringPlatform/tof_and_lidar/tof_and_lidar_logging_with_gps.py
