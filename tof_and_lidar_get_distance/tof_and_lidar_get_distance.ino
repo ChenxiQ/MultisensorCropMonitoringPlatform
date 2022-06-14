5c313bbb00e76bd646904603cb0983fa8c765c8e
ChenxiQ <cq53@cornell.edu>
2022-06-14 10:34:13 -0400

    Add two sensors getting distance

---
+++ tof_and_lidar_get_distance/tof_and_lidar_get_distance.ino
@@ -0,0 +1,58 @@
+#include <stdint.h>
+#include <Wire.h>
+#include "LIDARLite_v4LED.h"
+#include "SparkFun_VL53L1X.h"
+
+LIDARLite_v4LED myLidarLite;
+SFEVL53L1X distanceSensor;
+
+int distance;
+
+void setup() {
+    // Initialize Arduino serial port (for display of ASCII output to PC)
+    Serial.begin(115200);
+
+    // Initialize Arduino I2C (for communication to LidarLite)
+    Wire.begin();
+    digitalWrite(SCL, LOW);
+    digitalWrite(SDA, LOW);
+
+    myLidarLite.configure(0);
+    distanceSensor.setDistanceModeShort();
+
+    // Begin returns 0 on a good init
+    if (distanceSensor.begin() != 0) {
+        Serial.println("Sensor failed to begin. Please check wiring. Freezing...");
+        while (1)
+            ;
+    }
+    Serial.println("ToF sensor online!");
+}
+
+void loop() {
+    if (myLidarLite.getBusyFlag() == 0) {
+        myLidarLite.takeRange();
+        distance = myLidarLite.readDistance();
+        // Serial.print("Lidar distance: ");
+        Serial.print(distance);
+    }
+    delay(10);
+
+    Serial.print(" ");
+
+    // Write configuration bytes to initiate measurement
+    distanceSensor.startRanging();
+    while (!distanceSensor.checkForDataReady()) {
+        delay(1);
+    }
+    
+    // Get the result of the measurement from the sensor
+    int distance = distanceSensor.getDistance();
+    distanceSensor.clearInterrupt();
+    distanceSensor.stopRanging();
+
+    // Serial.print("ToF distance: ");
+    Serial.println(distance/10);
+
+    delay(100);
+}
